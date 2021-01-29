3af17eb3e7282a69cd564e30dc47eba9694cc62e
DatDoc <uncledrew2youngblood@gmail.com>
2021-01-29 16:14:29 +0700

    update

--- src/demo.py
+++ src/demo.py
@@ -87,10 +87,11 @@ def demo(opt):
           for i in range(len(dets[key])):
             if dets[key][i , 4] > 0.15:
                 ############# CHANGE THE EXPORT WAY ############################################
-                xmin, ymin, xmax, ymax = int(dets[key][i , 0]),int(dets[key][i , 1]),int(dets[key][i , 2]),int(dets[key][i , 3])
-                x, y = (xmin + (xmax - xmin) / 2) / 512, (ymin + (ymax - ymin) / 2) / 512
-                w, h = (xmax - xmin) / 512, (ymax - ymin) / 512
-                f.write('{} {} {} {} {} {}\n'.format(float(int(key) - 1), x, y, w, h, float(dets[key][i, 4])))
+                # xmin, ymin, xmax, ymax = int(dets[key][i , 0]),int(dets[key][i , 1]),int(dets[key][i , 2]),int(dets[key][i , 3])
+                # x, y = (xmin + (xmax - xmin) / 2) / 512, (ymin + (ymax - ymin) / 2) / 512
+                # w, h = (xmax - xmin) / 512, (ymax - ymin) / 512
+                # f.write('{} {} {} {} {} {}\n'.format(float(int(key) - 1), x, y, w, h, float(dets[key][i, 4])))
+                f.write('{} {} {} {} {} {}\n'.format(int(key) - 1,int(dets[key][i , 0]),int(dets[key][i , 1]),int(dets[key][i , 2]),int(dets[key][i , 3]), float(dets[key][i, 4])))
       f.close()
       
 if __name__ == '__main__':
